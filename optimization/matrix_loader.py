e4c5a23b19c035e293c92bc8d54a0d48f3d645df
Garrison Grogan <garrison.grogan@columbia.edu>
2018-07-30 10:58:17 -0400

    reworking for absorp and mult exp runs

--- optimization/matrix_loader.py
+++ optimization/matrix_loader.py
@@ -6,13 +6,31 @@ class OptMatrix(object):
         self.matrix = None
  
     #loads one experiment into self.matrix. Decides padding based on previous matrix or handle based on total exp num?
-    def load_S(self,interpolated_kinetic_sens,interpolated_tp_sens, interpolated_species_sens, observables_list):
+    def load_S(self, exp_num, list_of_interpolated_kinetic_sens,
+                              list_of_interpolated_tp_sens,
+                              list_of_interpolated_species_sens,
+                              list_of_absorbance,
+                              observables_list)
+    
+        for i in range(0,exp_num):
+            full_exp_matrix = self.load_single_exp(i,list_of_interpolated_kinetic_sens[i],
+                                                     list_of_interpolated_tp_sens[i],
+                                                     list_of_interpolated_species_sens[i],
+                                                     list_of_absorbance[i],
+                                                     observables_list)
+    
+    def load_single_exp(self, exp_num, interpolated_kinetic_sens,
+                                       interpolated_tp_sens,
+                                       interpolated_species_sens, 
+                                       observables_list):
         '''
         interpolated_kinetic_sens: list of 3 numpy arrays. Each array contains sheets representing one observable
                                    interpolated from experimental data
         interpolated tp sens: interpolated temperature and pressure sensitivities from some experimental data
         interpolated_species_sens: species sensitiviites listed by observables
         '''
+        '''
+        obs_matrix = None
         base_matrix = interpolated_kinetic_sens[0][0] #make a base to build the matrix off of
         for i,obs in enumerate(observables_list):
             #kinetic sensitiviities
@@ -25,13 +43,16 @@ class OptMatrix(object):
                                 #interpolated_tp_sens[1][obs].values.reshape(50,1)))
             #for spec_sen in interpolated_species_sens:
             #    base_matrix=np.hstack((base_matrix,spec_sen))
-            
+            # wrong way to loop
             if i == 0:
-                self.matrix= base_matrix
+                obs_matrix = base_matrix
             else:
-                self.matrix=np.vstack((self.matrix,base_matrix)) 
-                
+                obs_matrix = np.vstack((self.matrix,base_matrix)) 
+        #need to do abs, and handle padding,
+        #do calc to figure out size of the 0 chunks for mult exp
         return self.matrix
+        '''
+        pass
     def load_Y():
         pass
 
--- tests/matrix_loading_test.py
+++ tests/matrix_loading_test.py
@@ -34,10 +34,45 @@ int_tp_psen_against_experimental = test_tube.interpolate_experimental([test_tube
                                                                     test_tube.interpolate_physical_sensitivities(index=2)])
 int_spec_psen_against_experimental = test_tube.interpolate_experimental(pre_interpolated=test_tube.interpolate_species_sensitivities())
 
+
+test_p2 = pr.Processor('MSI/data/test_data/FFCM1.cti')
+test_tube2 = st.shockTube(pressure=1.74,
+                         temperature=1880,
+                         observables=['OH','H2O'],
+                         kineticSens=1,
+                         physicalSens=0,
+                         conditions={'H2O':.013,'O2':.0099,'H':.0000007,'Ar':0.9770993},
+                         initialTime=0,
+                         finalTime=0.5,
+                         thermalBoundary='Adiabatic',
+                         mechanicalBoundary='constant pressure',
+                         processor=test_p,
+                         save_timeHistories=1,
+                         save_physSensHistories=1)
+
+csv_paths2 = ['MSI/data/test_data/hong_h2o_4.csv','MSI/data/test_data/hong_oh_4.csv']
+exp_data2 = test_tube.importExperimentalData(csv_paths)
+
+test_tube2.run() #set up original time history
+int_ksens_exp_mapped2= test_tube.map_and_interp_ksens()#ksens is wiped on rerun so int it before
+test_tube.sensitivity_adjustment(temp_del = .01)
+test_tube.sensitivity_adjustment(pres_del = .01)
+test_tube.species_adjustment(.01) #do some sensitivity adjustments
+
+int_tp_psen_against_experimental2 = test_tube.interpolate_experimental([test_tube2.interpolate_physical_sensitivities(index=1),test_tube2.interpolate_physical_sensitivities(index=2)])
+int_spec_psen_against_experimental2 = test_tube2.interpolate_experimental(pre_interpolated=test_tube2.interpolate_species_sensitivities())
+
+list_of_interpolated_kinetic_sens = [int_ksens_exp_mapped,int_ksens_exp_mapped2]
+list_of_interpolated_tp_sens = [int_tp_psen_against_experimental,int_tp_psen_against_experimental2]
+list_of_interpolated_species_sens = [int_spec_psen_against_experimental,int_spec_psen_against_experimental2]
+
+
+
 mloader = ml.OptMatrix()
-S = mloader.load_S(int_ksens_exp_mapped,
-              int_tp_psen_against_experimental,
-              int_spec_psen_against_experimental,
-              test_tube.observables)
+S = mloader.load_S(2,list_of_interpolated_kinetic_sens,
+                     list_of_interpolated_tp_sens, 
+                     list_of_interpolated_species_sens,
+                     [],#need to do absorbance sens
+                     test_tube.observables)
 
 print(S)
