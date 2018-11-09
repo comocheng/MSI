4a9a88921b4c3069346e0094bc8b1fb922f31d41
Mark Barbet <mcb2261@columbia.edu>
2018-11-09 16:19:48 -0500

    Added init and settingJSRConditions functions

--- simulations/instruments/jsr_steadystate.py
+++ simulations/instruments/jsr_steadystate.py
@@ -17,11 +17,48 @@ class JSR_steadystate(sim.Simulation):
     
     def __init__(self,pressure:float,temperature:float,observables:list,
                  kineticSens:int,physicalSens:int,conditions:dict,thermalBoundary,mechanicalBoundary,
-                 processor:ctp.Processor=None,cti_path="",save_timeHistories:int=0, 
+                 processor:ctp.Processor=None,cti_path="", 
                  save_physSensHistories=0,moleFractionObservables:list=[],
                  absorbanceObservables:list=[],concentrationObservables:list=[],
                  fullParsedYamlFile:dict={}):
         
         sim.Simulation.__init__(self,pressure,temperature,observables,kineticSens,physicalSens,
                                 conditions,processor,cti_path)
+        self.thermalBoundary = thermalBoundary
+        self.mechanicalBoundary = mechanicalBoundary
+        self.kineticSensitivities= None
+        self.experimentalData = None
+        self.concentrationObservables = concentrationObservables
+        self.moleFractionObservables = moleFractionObservables
+        self.absorbanceObservables = absorbanceObservables
+        self.fullParsedYamlFile =  fullParsedYamlFile
         
+        if save_physSensHistories == 1:
+            self.physSensHistories = []
+        self.setTPX()
+        self.dk = [0]
+        
+        
+        
+    def settingJSRConditions(self):
+        '''
+        Determine the mechanical and thermal boundary conditions for a 
+        shock tube.
+        '''
+        
+        #assigning the thermal boundary variable
+        if re.match('[aA]diabatic',self.thermalBoundary):
+            energy = 'on'
+        elif re.match('[iI]sothermal',self.thermalBoundary):
+            energy = 'off'
+        else:
+            raise Exception('Please specify a thermal boundary condition, adiabatic or isothermal')
+        #assigning the mehcanical boundary variable 
+        if re.match('[Cc]onstant [Pp]ressure',self.mechanicalBoundary):
+            mechBoundary = 'constant pressure'
+        elif re.match('[Cc]onstant [Vv]olume',self.mechanicalBoundary):
+            mechBoundary = 'constant volume'
+        else:
+            raise Exception('Please specifiy a mehcanical boundary condition, constant pressure or constant volume')
+        #return the thermal and mechanical boundary of the shock tube 
+        return energy,mechBoundary
